350fcafcb93d5190d1d636bfefb80a4086911db7
KennyAle <kennyviver@gmail.com>
2024-09-17 11:41:31 -0700

    Exercise 3

---
+++ flowcharts/js/exercise-3.js
@@ -0,0 +1,11 @@
+let a = 4
+let b = 5
+let c = 9
+
+if (a > b && a > c) {
+    console.log(`a (${a}) is the GOAT`)
+} else if ( b > a && b > c ) {
+    console.log(`b (${b}) is the GOAT`)
+} else {
+    console.log(`c (${c}) is the GOAT`)
+}
